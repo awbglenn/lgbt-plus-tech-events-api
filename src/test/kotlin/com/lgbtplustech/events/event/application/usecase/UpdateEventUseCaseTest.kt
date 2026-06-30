e01b486a903083efabab3e397ef2fee7c8e71dc4
Glenn A WB <awbglenn@gmail.com>
2026-06-30 14:46:03 +0200

    Small refactor

--- src/test/kotlin/com/lgbtplustech/events/event/application/usecase/UpdateEventUseCaseTest.kt
+++ src/test/kotlin/com/lgbtplustech/events/event/application/usecase/UpdateEventUseCaseTest.kt
@@ -5,7 +5,7 @@ import com.lgbtplustech.events.event.application.exception.EventNotFoundExceptio
 import com.lgbtplustech.events.event.application.port.UpdateEvent
 import com.lgbtplustech.events.testing.FakeEventRepository
 import com.lgbtplustech.events.testing.testEvent
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.time.Clock
@@ -15,48 +15,46 @@ import java.util.UUID
 
 class UpdateEventUseCaseTest {
 
+    private val clock = Clock.fixed(
+        Instant.parse("2026-06-02T10:00:00Z"),
+        ZoneOffset.UTC
+    )
+
     @Test
-    fun `updates event`() {
+    fun `updates event fields`() {
         val repository = FakeEventRepository()
         val event = testEvent()
         repository.save(event)
-        val clock = Clock.fixed(
-            Instant.parse("2026-06-02T10:00:00Z"),
-            ZoneOffset.UTC
-        )
         val updateEvent: UpdateEvent = UpdateEventUseCase(repository, clock)
 
         updateEvent.execute(
             UpdateEventCommand(
                 id = event.id,
                 title = "Updated title",
-                description = "Updated description",
-                startsAt = Instant.parse("2026-08-01T18:30:00Z"),
-                endsAt = Instant.parse("2026-08-01T21:00:00Z"),
-                venueName = "Updated Venue",
-                venueAddress = "Updated Address",
                 capacity = 100
             )
         )
+
         val updatedEvent = repository.findById(event.id)
 
         assertEquals("Updated title", updatedEvent?.title)
         assertEquals(100, updatedEvent?.capacity)
+        assertEquals(event.description, updatedEvent?.description)
         assertEquals(Instant.parse("2026-06-02T10:00:00Z"), updatedEvent?.updatedAt)
     }
 
     @Test
     fun `throws when event does not exist`() {
         val repository = FakeEventRepository()
-        val clock = Clock.fixed(
-            Instant.parse("2026-06-02T10:00:00Z"),
-            ZoneOffset.UTC
-        )
-
         val updateEvent: UpdateEvent = UpdateEventUseCase(repository, clock)
 
         assertThrows<EventNotFoundException> {
-            updateEvent.execute(UpdateEventCommand(id = UUID.randomUUID(), title = "new title"))
+            updateEvent.execute(
+                UpdateEventCommand(
+                    id = UUID.randomUUID(),
+                    title = "Updated title"
+                )
+            )
         }
     }
 }
